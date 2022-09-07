45a9fe2115a3119881fa016c780a8545561e36dc
RanganeniJaswanth <ranganenijaswanth4065.sse@saveetha.com>
2022-09-07 14:00:27 +0530

    Add files via upload

--- pattern3.python.py
+++ pattern3.python.py
@@ -1,6 +1,6 @@
-n =int(input("Enter the number of rows"))
-for i in range(1, n+1):
-    for j in range(1, i+1):
-            print(j, end=" ")
+for i in range(1,6):
+    for j in range (1,6-i):
+        print(end=" ")
+    for k in range(1,i+1):
+        print(k,end=" ")
     print()
-        
