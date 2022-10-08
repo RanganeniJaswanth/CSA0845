e821e7fdd641a4c986662d4a34b5f1250d2eaa6c
RanganeniJaswanth <ranganenijaswanth4065.sse@saveetha.com>
2022-10-08 13:29:52 +0530

    Delete greatest of number.py

--- greatest of number.py
+++
@@ -1,9 +0,0 @@
-# author jaswanth 065 05\09\22
-def max_of_two(x,y):
-    if x > y:
-         return x
-         return y
-    return z
-def max_of_three( x, y, z):
-    return max_of_two(x, max_of_two(y, z))
-print(max_of_three(99,86,77))
