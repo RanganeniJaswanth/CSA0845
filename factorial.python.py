cf6f97e4232d9fa840bee75a1942ddbcdb190126
RanganeniJaswanth <ranganenijaswanth4065.sse@saveetha.com>
2022-10-08 13:29:30 +0530

    Delete factorial.python.py

--- factorial.python.py
+++
@@ -1,7 +0,0 @@
-# author jaswanth 065 06\09\22
-number = int(input("enter the number:"))
-fact=1
-
-for i in range(1,number +1):
-  fact = fact*i
-print("the factorial of %d = %d" %(number, fact))            
