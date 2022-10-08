12d026dd3dae959fcf8afab3f3fb99125f03085a
RanganeniJaswanth <ranganenijaswanth4065.sse@saveetha.com>
2022-10-08 10:53:03 +0530

    Delete day-2 1.2 leap year or not.py.py

--- day-2 1.2 leap year or not.py.py
+++
@@ -1,8 +0,0 @@
-Year=int(input('Enter the Year: '))
-date=int(input('enter date:'))
-if (Year%400==0) and (Year%100!=0):
-  print("{} is a leap year".format(Year))
-elif (Year%4==0):
-  print("{} is a leap year".format(Year))
-else:
-  print("{} is not a leap year".format(Year))
